a07cea9a1bdff6f8a8ca396c879b50cabc27b803
larsenv <larsen@weborican.com>
2017-08-21 11:06:41 -0500

    Fixed get

--- index.php
+++ index.php
@@ -5,12 +5,12 @@ if (isset($_GET["l"]) || isset($_GET["c"]))
 {
 	if ($_GET["l"] == "fr")
 	{
-		if ($_GET["C"] == "US")
+		if ($_GET["c"] == "US")
 		{
 			header("Location: http://rs.wii.rc24.xyz/content/US_FR/recommended_links_FR.html");
 		}
 
-		else if ($_GET["C"] == "CA")
+		else if ($_GET["c"] == "CA")
 		{
 			header("Location: http://rs.wii.rc24.xyz/content/CA_FR/recommended_links_FR.html");
 		}
@@ -23,7 +23,7 @@ if (isset($_GET["l"]) || isset($_GET["c"]))
 
 	else if ($_GET["l"] == "es")
 	{
-		if ($_GET["C"] == "US")
+		if ($_GET["c"] == "US")
 		{
 			header("Location: http://rs.wii.rc24.xyz/content/US_SP/recommended_links_SP.html");
 		}
@@ -36,12 +36,12 @@ if (isset($_GET["l"]) || isset($_GET["c"]))
 
 	else if ($_GET["l"] == "en")
 	{
-		if ($_GET["C"] == "US")
+		if ($_GET["c"] == "US")
 		{
 			header("Location: http://rs.wii.rc24.xyz/content/US_EN/recommended_links.html");
 		}
 
-		else if ($_GET["C"] == "CA")
+		else if ($_GET["c"] == "CA")
 		{
 			header("Location: http://rs.wii.rc24.xyz/content/CA_EN/recommended_links.html");
 		}
@@ -56,5 +56,10 @@ if (isset($_GET["l"]) || isset($_GET["c"]))
 	{
 		header("Location: http://rs.wii.rc24.xyz/content/LA_EN/recommended_links.html");
 	}
+}
+
+else
+{
+	header("Location: http://rs.wii.rc24.xyz/content/LA_EN/recommended_links.html");
 }
 ?>
